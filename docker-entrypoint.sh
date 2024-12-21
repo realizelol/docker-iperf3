6f6a9e72e21db3e6c5d2a9d4a0f22f5537303412
realizelol <bsw.bsw@gmx.de>
2024-12-21 13:07:48 +0100

    [Test] No bash installed?!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 #set -x ## debug
 
 # change repo to 'latest-stable'
-sed -i -e 's/v[0-9]\.[0-9]\{2\}/latest-stable/g' /etc/apk/repositories
+sed -i 's/v[0-9]\.[0-9]\{2\}/latest-stable/g' /etc/apk/repositories
 
 # upgrade packages
 apk upgrade -Ua --prune && apk -v cache purge && sync
